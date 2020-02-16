2d31c924e5399d19ee999f009b134a64e1347175
Caleb Bassi <calebjbassi@gmail.com>
2020-02-16 08:08:10 -0800

    0.5.0

--- ytop.rb
+++ ytop.rb
@@ -1,13 +1,13 @@
 class Ytop < Formula
   desc "A TUI system monitor written in Rust"
   homepage "https://github.com/cjbassi/ytop"
-  version "0.4.3"
-  url "https://github.com/cjbassi/ytop/releases/download/0.4.3/ytop-0.4.3-x86_64-apple-darwin.tar.gz"
-  sha256 "f30b84648960cc5c28a707f9251d2f9514535b1a5f253c1ad8ea6cd772c9ca70"
+  version "0.5.0"
+  url "https://github.com/cjbassi/ytop/releases/download/0.5.0/ytop-0.5.0-x86_64-apple-darwin.tar.gz"
+  sha256 "e533a4f7d65734666e1d1d41f4c07e607b83d2381dad583fbda3d20dfd54530d"
 
   if OS.linux?
-    url "https://github.com/cjbassi/ytop/releases/download/0.4.3/ytop-0.4.3-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "c0398ede74b700bcdd763f85d534b1d41840095f11b361b7de5b3006d0553c5f"
+    url "https://github.com/cjbassi/ytop/releases/download/0.5.0/ytop-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "8d95ab6b1a4b13e9fab0ee3e5eaa4c24a0cd9c823950f4481b679f843e29c660"
   end
 
   def install
